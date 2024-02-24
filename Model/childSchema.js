d89e843b62e99a4c8e90de7723d1123381329658
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-02-24 02:01:52 +0200

    Add auto-increment functionality to child and class IDs

--- Model/childSchema.js
+++ Model/childSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 const AddressSchema = mongoose.Schema(
   {
@@ -9,20 +10,24 @@ const AddressSchema = mongoose.Schema(
   { _id: false }
 );
 
-const ChildSchema = new mongoose.Schema({
-  _id: {
-    type: Number,
-    unique: true,
-    index: true,
+const ChildSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: Number,
+      unique: true,
+      index: true,
+    },
+    fullname: String,
+    level: {
+      type: String,
+      enum: ["PreKG", "KG1", "KG2"],
+      required: true,
+    },
+    image: { type: String, required: true },
+    adddress: AddressSchema,
   },
-  fullname: String,
-  level: {
-    type: String,
-    enum: ["PreKG", "KG1", "KG2"],
-    required: true,
-  },
-  image: { type: String, required: true },
-  adddress: AddressSchema,
-});
+  { _id: false }
+);
+ChildSchema.plugin(AutoIncrement, { id: "childId", inc_field: "_id" });
 
 module.exports = new mongoose.model("Child", ChildSchema);
--- Model/classSchema.js
+++ Model/classSchema.js
@@ -1,16 +1,25 @@
 const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
-const classSchema = new mongoose.Schema({
-  _id: Number,
-  name: String,
-  supervisor: {
-    type: mongoose.ObjectId,
-    ref: "Teacher",
+const classSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: Number,
+      unique: true,
+      index: true,
+    },
+    name: String,
+    supervisor: {
+      type: mongoose.ObjectId,
+      ref: "Teacher",
+    },
+    children: {
+      type: [Number],
+      ref: "Child",
+    },
   },
-  children: {
-    type: [Number],
-    ref: "Child",
-  },
-});
+  { _id: false }
+);
+classSchema.plugin(AutoIncrement,{id:"classId",inc_field:"_id"})
 
 module.exports = mongoose.model("Class", classSchema);
