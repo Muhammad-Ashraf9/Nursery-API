f16fad65ead0c7f1ab99c11091efef7de051f8ad
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-02-24 03:12:24 +0200

    Add new routes and controller methods for teacher management

--- Controller/teacherController.js
+++ Controller/teacherController.js
@@ -1,9 +1,14 @@
+const fs = require("fs");
+
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+
 require("dotenv").config();
 
 const Teacher = require("../Model/teacherSchema");
-exports.getAllTeachers = (req, res) => {
+const Class = require("../Model/classSchema");
+
+exports.getAllTeachers = (req, res, next) => {
   Teacher.find()
     .then((teachers) => {
       res.status(200).json({ data: teachers, message: "take all teachers" });
@@ -27,7 +32,7 @@ exports.addNewTeacher = async (req, res, next) => {
     const token = jwt.sign(
       {
         id: newTeacherData._id,
-        fullname: newTeacherData.name,
+        fullname: newTeacherData.fullname,
         role: "teacher",
       },
       process.env.SECRET_KEY,
@@ -40,7 +45,43 @@ exports.addNewTeacher = async (req, res, next) => {
     next(error);
   }
 };
-exports.updateTeacherData = (req, res) => {
+exports.updateTeacherData = (req, res, next) => {
   const id = req.body.id;
   res.status(201).json({ id, message: "update teacher data" });
+};
+exports.deleteTeacherById = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const teacher = await Teacher.findByIdAndDelete(id);
+    //delete image from uploads folder
+    fs.unlink(teacher.image, (err) => {
+      if (err) throw err;
+    });
+    res.status(200).json({ teacher, message: "Teacher deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getAllClassSupervisors = async (req, res, next) => {
+  try {
+    const supervisors = await Class.find()
+      .populate("supervisor")
+      .populate("children");
+    res
+      .status(200)
+      .json({ data: supervisors, message: "take all supervisors" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getTeacherById = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const teacher = await Teacher.findById(id);
+    res.status(200).json({ data: teacher, message: "take teacher by id" });
+  } catch (error) {
+    next(error);
+  }
 };
--- Routes/teacherRoute.js
+++ Routes/teacherRoute.js
@@ -3,10 +3,21 @@ const {
   getAllTeachers,
   addNewTeacher,
   updateTeacherData,
+  deleteTeacherById,
+  getAllClassSupervisors,
+  getTeacherById,
 } = require("../Controller/teacherController");
 
 const router = express.Router();
 
-router.route("/teachers").get(getAllTeachers).put(updateTeacherData);
+router
+  .route("/teachers")
+  .get(getAllTeachers)
+  .put(updateTeacherData)
+  .delete(deleteTeacherById);
+
+router.get("/teachers/supervisors", getAllClassSupervisors);
+
+router.get("/teachers/:id", getTeacherById);
 
 module.exports = router;
