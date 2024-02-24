4ab064d932e7c23bc8c6aa0b4cea66d3ee748b3b
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-02-24 10:32:39 +0200

    Fix file deletion bug in validator middleware

--- Middlewares/validation/validator.js
+++ Middlewares/validation/validator.js
@@ -1,8 +1,19 @@
 const { validationResult } = require("express-validator");
+const { removeFile, getImageFullPath } = require("../../helper");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = (req, res, next) => {
   let result = validationResult(req);
   if (result.errors.length >= 1) {
+    if (req.file) {
+      fs.unlink(
+        path.join(__dirname, "..", "..", "images", req.file.filename),
+        (err) => {
+          if (err) throw err;
+        }
+      );
+    }
     let errorMessages = result.errors.reduce(
       (current, error) => current + error.msg + " ",
       " "
@@ -22,4 +33,4 @@ exports.imageValidation = (req, res, next) => {
     next(error);
   }
   next();
-}
+};
